2b04b04575454c48ed6c37b9063c6af919ac3cb4
chenxiaru <1062640058@qq.com>
2019-08-07 23:37:08 +0800

    由模块引出npm的概念

--- 1.require/usePerson.js
+++ 1.require/usePerson.js
@@ -2,3 +2,6 @@
 var personData = require('./person.js')
 console.log('personData', personData)
 console.log('personData', personData.name)
+
+// global是node的全局对象,类似浏览器中的 window
+console.log('global', global)
