1761e5924f407a4ed90b4f33433d15e8b64079df
chenxiaru <1062640058@qq.com>
2019-08-16 16:27:33 +0800

    使用fs模块读写文件

--- 3.http/http.js
+++ 3.http/http.js
@@ -1,5 +1,13 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-07 23:50:31
+ * @LastEditTime: 2019-08-16 16:22:37
+ * @LastEditors: Please set LastEditors
+ */
 // 引入 http 模块
 let http = require('http')
+let fs = require('fs')
 
 function serve (request, response) {
   console.log('请求方法', request.method)
@@ -9,13 +17,17 @@ function serve (request, response) {
   response.statusCode = 200
   response.setHeader('111', 111) // 响应头
   response.setHeader('Content-Type', 'text/html;charset=utf-8')
-  response.write(new Date().toString())  // 响应体
-  response.end()  //响应结束
+  fs.readFile('index.html', (err, data) => {
+    response.write(data)  //将读取的 index.html 内容写入响应体
+    response.end()
+  })
+  // response.write(new Date().toString())  // 响应体
+  // response.end()  //响应结束
 }
 
 // 执行响应
 let server = http.createServer(serve) 
 
 // 监听
-server.listen(8000, 'localhost')
+server.listen(8000, '10.187.60.56')
 
